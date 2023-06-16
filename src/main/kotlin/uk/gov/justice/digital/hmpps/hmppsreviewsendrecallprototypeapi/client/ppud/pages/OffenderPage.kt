d8388c8db9e3ad4022a881b25f63cf7845ebc110
Darren Walker <Darren.Walker@digital.justice.gov.uk>
2023-06-16 17:46:11 +0100

    MRD-1495 Refactor treeview behaviour

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsreviewsendrecallprototypeapi/client/ppud/pages/OffenderPage.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsreviewsendrecallprototypeapi/client/ppud/pages/OffenderPage.kt
@@ -1,12 +1,15 @@
 package uk.gov.justice.digital.hmpps.hmppsreviewsendrecallprototypeapi.client.ppud.pages
 
 import org.openqa.selenium.By
+import org.openqa.selenium.Dimension
+import org.openqa.selenium.OutputType
+import org.openqa.selenium.Point
+import org.openqa.selenium.Rectangle
 import org.openqa.selenium.WebDriver
 import org.openqa.selenium.WebElement
 import org.openqa.selenium.support.FindBy
 import org.openqa.selenium.support.PageFactory
 import uk.gov.justice.digital.hmpps.hmppsreviewsendrecallprototypeapi.client.ppud.PpudClient
-import uk.gov.justice.digital.hmpps.hmppsreviewsendrecallprototypeapi.client.ppud.sleepIfRequired
 
 class OffenderPage(private val driver: WebDriver) {
 
@@ -47,44 +50,18 @@ class OffenderPage(private val driver: WebDriver) {
     driver.get("$ppudUrl/Offender/PersonalDetails.aspx?data=$offenderId")
   }
 
-  suspend fun navigateToNewRecallFor(sentenceDate: String, releaseDate: String) {
+  fun navigateToNewRecallFor(sentenceDate: String, releaseDate: String) {
     val navigationElement = driver.findElement(By.id("navigation"))
-    val navigationTreeView = navigationElement.findElement(By.id("T_ctl00treetvOffender"))
+    val navigationTreeView = TreeView(navigationElement.findElement(By.id("T_ctl00treetvOffender")))
 
-    val sentences = navigationTreeView.findElementWithText("Sentences")
-    val sentencesChildren = expandNode(sentences)
-    val sentence = sentencesChildren.findElement(By.xpath("//*[contains(text(), '$sentenceDate')]"))
-    val sentenceChildren = expandNode(sentence)
-
-    val releases = sentenceChildren.findElementWithText("Releases")
-    val releasesChildren = expandNode(releases)
-    val release = releasesChildren.findElementWithTextContaining(releaseDate)
-    val releaseChildren = expandNode(release)
-
-    val recalls = releaseChildren.findElementWithTextContaining("Recalls")
-    val recallsChildren = expandNode(recalls)
-    val newRecallElement = recallsChildren.findElementWithTextContaining("New")
-    sleepIfRequired()
-    newRecallElement.click()
-
-    // We perhaps want to write this like this...
-    //    findTreeView("tree_tvOffender")
-    //      .expandNodeWithText("Sentences")
-    //      .expandNodeWithTextContaining(sentenceDate)
-    //      .expandNodeWithText("Releases")
-    //      .expandNodeWithTextContaining(releaseDate)
-    //      .expandNodeWithText("Recalls")
-    //      .findNodeWithTextContaining("New")
-    //      .click()
-  }
-
-  private fun expandNode(textNode: WebElement): WebElement {
-    val expansionElement = textNode.findElement(By.xpath("../following-sibling::div"))
-    if (expansionElement.isDisplayed.not()) {
-      val expanderImage = textNode.findElement(By.xpath("../img[@imgtype='exp']"))
-      expanderImage.click()
-    }
-    return expansionElement
+    navigationTreeView
+      .expandNodeWithText("Sentences")
+      .expandNodeWithTextContaining(sentenceDate)
+      .expandNodeWithText("Releases")
+      .expandNodeWithTextContaining(releaseDate)
+      .expandNodeWithTextContaining("Recalls")
+      .findNodeWithTextContaining("New")
+      .click()
   }
 }
 
@@ -92,10 +69,113 @@ private fun WebElement?.getValue(): String {
   return this?.getAttribute("value")?.trim() ?: ""
 }
 
-private fun WebElement.findElementWithText(text: String): WebElement {
-  return this.findElement(By.xpath(".//*[text()='$text']"))
-}
+class TreeView(private val element: WebElement) : WebElement {
 
-private fun WebElement.findElementWithTextContaining(text: String): WebElement {
-  return this.findElement(By.xpath(".//*[contains(text(), '$text')]"))
+  fun expandNodeWithText(text: String): TreeViewNode {
+    return TreeViewNode(this).expandNodeWithText(text)
+  }
+
+  override fun findElements(by: By?): MutableList<WebElement> {
+    return element.findElements(by)
+  }
+
+  override fun findElement(by: By?): WebElement {
+    return element.findElement(by)
+  }
+
+  override fun <X : Any?> getScreenshotAs(target: OutputType<X>?): X {
+    return element.getScreenshotAs(target)
+  }
+
+  override fun click() {
+    element.click()
+  }
+
+  override fun submit() {
+    element.submit()
+  }
+
+  override fun sendKeys(vararg keysToSend: CharSequence?) {
+    element.sendKeys()
+  }
+
+  override fun clear() {
+    element.clear()
+  }
+
+  override fun getTagName(): String {
+    return element.tagName
+  }
+
+  override fun getAttribute(name: String?): String {
+    return element.getAttribute(name)
+  }
+
+  override fun isSelected(): Boolean {
+    return element.isSelected
+  }
+
+  override fun isEnabled(): Boolean {
+    return element.isEnabled
+  }
+
+  override fun getText(): String {
+    return element.text
+  }
+
+  override fun isDisplayed(): Boolean {
+    return element.isDisplayed
+  }
+
+  override fun getLocation(): Point {
+    return element.location
+  }
+
+  override fun getSize(): Dimension {
+    return element.size
+  }
+
+  override fun getRect(): Rectangle {
+    return element.rect
+  }
+
+  override fun getCssValue(propertyName: String?): String {
+    return element.getCssValue(propertyName)
+  }
+
+  class TreeViewNode(val element: WebElement) {
+
+    fun expandNodeWithText(text: String): TreeViewNode {
+      return TreeViewNode((expandNode(findNodeWithText(text))))
+    }
+
+    fun expandNodeWithTextContaining(text: String): TreeViewNode {
+      return TreeViewNode(expandNode(findNodeWithTextContaining(text)))
+    }
+
+    private fun findNodeWithText(text: String): TreeViewNode {
+      return TreeViewNode(element.findElement(By.xpath(".//*[text()='$text']")))
+    }
+
+    fun findNodeWithTextContaining(text: String): TreeViewNode {
+      return TreeViewNode(element.findElement(By.xpath(".//*[contains(text(), '$text')]")))
+    }
+
+    private fun expandNode(textNode: TreeViewNode): WebElement {
+      val expansionElement = textNode.findElement(By.xpath("../following-sibling::div"))
+      if (expansionElement.isDisplayed.not()) {
+        val expanderImage = textNode.findElement(By.xpath("../img[@imgtype='exp']"))
+        expanderImage.click()
+      }
+      return expansionElement
+    }
+
+    fun click() {
+      element.click()
+    }
+
+    private fun findElement(by: By?): WebElement {
+      return element.findElement(by)
+    }
+  }
 }
